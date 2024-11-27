044eab0445a159ee7ef1d4d5ebc4ef36da5f2011
Meer Uzair <meer.uxair007@gmail.com>
2024-11-28 00:58:47 +0530

    added swagger documentation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,10 +5,14 @@ import express, { Request, Response, NextFunction } from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import helmet from "helmet";
+import rateLimiter from "express-rate-limit";
+import cors from "cors";
+
 import { prismaClient } from "./db";
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
-
+import swaggerUi from "swagger-ui-express";
+import { openApiSpec } from "./openApiSpec";
 // Routers
 import authRouter from "./routes/auth.routes";
 import userRouter from "./routes/user.routes";
@@ -34,11 +38,26 @@ app.use(cookieParser(process.env.COOKIE_SECRET));
 // app.use(express.static(resolve(__dirname, "./client/dist"))); // Uncomment if you have a frontend
 
 // Security
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
 app.use(helmet());
+app.use(cors());
 
 // Routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
+
+// Remove this in production
+app.use("/documentation", swaggerUi.serve, swaggerUi.setup(openApiSpec));
+
+app.get("*", (req, res) => {
+  res.redirect("/documentation");
+});
 
 // Serve static files in production
 // Uncomment the below line if you have a frontend to serve in production
@@ -48,7 +67,6 @@ app.use("/api/v1/users", userRouter);
 
 // Error handling
 app.use(notFoundMiddleware);
-// app.use(errorHandlerMiddleware);
 app.use(errorHandlerMiddleware);
 
 // Port
