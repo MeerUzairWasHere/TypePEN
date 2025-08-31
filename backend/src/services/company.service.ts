1761b2710521e1e1450547fa890722278f841c28
Meer Uzair <meer.uxair007@gmail.com>
2025-08-31 23:19:49 +0530

    Update company.service.ts

--- backend/src/services/company.service.ts
+++ backend/src/services/company.service.ts
@@ -1,13 +1,57 @@
 import { prismaClient } from "../db";
-import { AddCompanyInput } from "../types";
+import { ConflictError, NotFoundError } from "../errors";
+import { CompanyInput } from "../types";
+import { CompanyInfo } from "../types/email.types";
 
 export class CompanyService {
-  async createCompany(data: AddCompanyInput) {
+  async createCompany({ data }: { data: CompanyInput }) {
+    const companyAlreadyExists = await prismaClient.company.findFirst();
+
+    if (companyAlreadyExists) {
+      throw new ConflictError("Company already exists");
+    }
+
     const company = await prismaClient.company.create({
       data,
     });
 
     return company;
+  }
+
+  async getCompany(): Promise<CompanyInfo> {
+    const company = await prismaClient.company.findFirst();
+
+    if (!company) {
+      throw new NotFoundError(
+        `Company does not exist, Please create a new one.`
+      );
+    }
+
+    return company;
+  }
+
+  async updateComany({
+    companyId,
+    data,
+  }: {
+    companyId: string;
+    data: CompanyInput;
+  }) {
+    await this.getCompany();
+
+    const company = await prismaClient.company.update({
+      data: data,
+      where: {
+        id: companyId,
+      },
+    });
+
+    return company;
+  }
+
+  async deleteCompany() {
+    await prismaClient.company.deleteMany();
+    return;
   }
 }
 
